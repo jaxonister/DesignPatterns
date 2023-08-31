403d741f44512ac39ccf87403a40ed3f7881c24c
jaxon <1140341230@qq.com>
2023-08-31 19:02:13 +0800

    add 过滤器模式

--- 过滤器模式/示例1/filter_model.py
+++ 过滤器模式/示例1/filter_model.py
@@ -1,43 +1,34 @@
 """
 __author__ = jaxon
-__time__ = 2023/8/30 19:19
+__time__ = 2023/8/31 16:33
 __desc__ = ''
 """
 from abc import ABC, abstractmethod
 
 
-# --------------抽象处理者---------------
 class AbcCheckHandler(ABC):
 
     @abstractmethod
-    def check_password(self, password):
+    def check_password(self, data, password):
         pass
 
     @abstractmethod
-    def set_next(self, handler):
-        pass
+    def is_deal(self, handler):
+        ...
 
 
-# ---------------具体处理---------------
 class CheckHanlderBase(AbcCheckHandler):
     _next_handler = None
 
-    def set_next(self, handler):
-        self._next_handler = handler
+    def is_deal(self, handler):
+        ...
 
-    @abstractmethod
-    def _check(self, password):
-        pass
-
-    def check_password(self, password):
-        status, msg = self._check(password)
-        if status:
-            if self._next_handler:
-                return self._next_handler.check_password(password)
-            else:
-                return True, ''
-        else:
-            return status, msg
+    def check_password(self, data: list, password):
+        for x in data:
+            status, msg = x.is_deal(password)
+            if not status:
+                return msg
+            return status
 
 
 class LengthCheckHandler(CheckHanlderBase):
@@ -45,7 +36,7 @@ class LengthCheckHandler(CheckHanlderBase):
     专门检查长度
     """
 
-    def _check(self, password):
+    def is_deal(self, password):
         if len(password) > 6:
             return True, ''
         return False, '长度错误'
@@ -56,7 +47,7 @@ class CaseCheckHandler(CheckHanlderBase):
     专门检查大小写
     """
 
-    def _check(self, password):
+    def is_deal(self, password):
         has_lower = False
         has_upper = False
 
@@ -73,14 +64,13 @@ class CaseCheckHandler(CheckHanlderBase):
 
 
 class SpecialCheckHandler(CheckHanlderBase):
-    def _check(self, password):
+    def is_deal(self, password):
         for letter in ('!', '@', '#', '$'):
             if letter in password:
                 return True, ''
         return False, '未包含特殊字符'
 
 
-# ---------调用------------
 def check_password(password):
     # 检查长度
     len_checker = LengthCheckHandler()
@@ -88,16 +78,10 @@ def check_password(password):
     case_checker = CaseCheckHandler()
     # 检查特殊字符
     special_checker = SpecialCheckHandler()
-    # 设置调用链
-    # 检查长度> 检查大小写 > 检查特殊字符
-    len_checker.set_next(case_checker)
-    case_checker.set_next(special_checker)
+    ins = [len_checker, case_checker, special_checker]
 
-    status, msg = len_checker.check_password(password)
-    if status:
-        print(f"{password} 检查通过")
-    else:
-        print(f"{password} 检查不通过: {msg}")
+    r = CheckHanlderBase().check_password(ins, password)
+    return r
 
 
 if __name__ == '__main__':
